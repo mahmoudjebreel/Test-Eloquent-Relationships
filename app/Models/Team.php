94df03c774ee14a4d89e152518233723eb20705b
noaman jebreel <m.jebreel22@gmail.com>
2023-05-22 14:10:39 +0300

    first commit

---
+++ app/Models/Team.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'size', 'country_id'];
+
+    public function users()
+    {
+        // TASK: fix this by adding some extra code
+        return $this->belongsToMany(User::class, 'team_user', 'users_id','team_id');
+    }
+}
